394a4cf5d1eb602c49bba5cfe67099404e40b363
Daiver <ra22341@ya.ru>
2013-09-17 00:03:54 +0400

    minor changes

---
+++ misc/grab_sample.py
@@ -0,0 +1,14 @@
+from grab import Grab
+
+pageaddr = 'http://yandex.ru/yandsearch?lr=193&text=4710937382310' 
+pageaddr = 'http://market.yandex.ru/model.xml?text=4710937382310&srnum=4&modelid=7880758&hid=91491' 
+pageaddr = 'http://market.yandex.ru/product/7880758/reviews'
+pageaddr ='http://google.com/search?hl=en&as_q=%s&num=%s&as_qdr=%s'%('4710937382310',str(10),'')
+#pageaddr = 'http://www.google.ru/?q=4710937382310#newwindow=1&psj=1&q=4710937382310' 
+
+g = Grab(log_file='out.html')
+g.go(pageaddr)
+try:
+    print g.xpath_text('//a[@class="b-serp-item__title-link"]')
+except:
+    print g.xpath_text('//*')
