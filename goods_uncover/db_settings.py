a70feaad4e1b31e952a601e55fca064f4383eb3e
alex <alex-fkn@yandex.ru>
2013-09-17 17:02:24 +0400

    remove dbset

--- goods_uncover/db_settings.py
+++
@@ -1,9 +0,0 @@
-db_set = {
-    'engine'    : 'mysql',
-    'name'      : 'goods_uncover',
-    'user'      : 'root',
-    'password'  : '123456',
-    'host'      : '',
-    'port'      : '',
-    'debug'     : True,
-}
