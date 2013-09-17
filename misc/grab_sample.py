1e02a59a7387c8b001312daf9a27deb10d110bc4
Daiver <ra22341@ya.ru>
2013-09-17 18:10:45 +0400

    minor changes

--- misc/grab_sample.py
+++ misc/grab_sample.py
@@ -6,6 +6,8 @@ pageaddr = 'http://market.yandex.ru/product/7880758/reviews'
 pageaddr ='http://google.com/search?hl=en&as_q=%s&num=%s&as_qdr=%s'%('4710937382310',str(10),'')
 #pageaddr = 'http://www.google.ru/?q=4710937382310#newwindow=1&psj=1&q=4710937382310' 
 
+print pageaddr
+exit()
 g = Grab(log_file='out.html')
 g.go(pageaddr)
 #print g.xpath_text('//a[@class="b-serp-item__title-link"]')
