3bc1f4d11beb7f72ffe729f6a9b31340d1febd1c
Daiver <ra22341@ya.ru>
2013-09-18 00:37:21 +0400

    second version os ya_market

--- misc/ya_market.py
+++ misc/ya_market.py
@@ -1,18 +1,23 @@
 import sys
 from grab import Grab
 
-def google_it(q, num=10):
+def ym_search(q):
     #pageaddr ='http://google.com/search?hl=en&as_q=%s&num=%s&as_qdr=%s' % ('+'.join(q.split()), str(num),'')
     pageaddr ='http://market.yandex.ru/search.xml?text=%s' % ('+'.join(q.split()))
+    print pageaddr
     g = Grab()
     g.go(pageaddr)
-    titles = g.doc.select('//div/h3').text_list()
-    hrefs = g.doc.select('//div/h3/a/@href').text_list()
-    return zip(titles, hrefs)
+    title = g.doc.select('//div[@class="b-offers b-offers_type_guru b-offers_type_guru_mix"]/div/h3/a').text()
+    href = g.doc.select('//div[@class="b-offers b-offers_type_guru b-offers_type_guru_mix"]/div/h3/a/@href').text()
+    print title
+    print href
+
+    #hrefs = g.doc.select('//div/h3/a/@href').text_list()
+    #return zip(titles, hrefs)
 
 if __name__ == '__main__':
-    num = int(sys.argv[2]) if len(sys.argv) > 2 else 10
     if len(sys.argv) > 1:
-        print google_it(sys.argv[1], num)
+        print ym_search(sys.argv[1])
     else:
-        print google_it("ya.ru", 10)
+        print ym_search("4710937382310")
+        #print ym_search("4605922006695")
