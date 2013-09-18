c98abc108a74e19363eac7cce651c5338df3b66e
alex <alex-fkn@yandex.ru>
2013-09-18 19:19:31 +0400

    new page

--- file_manage/urls.py
+++ file_manage/urls.py
@@ -3,6 +3,7 @@ from django.conf.urls import patterns, include, url
 
 urlpatterns = patterns('file_manage.views',
                        url(r'all/$', 'allfiles', name='file_manage.allfiles'),
+                       url(r'last/$', 'last_barcode', name='file_manage.last_barcode'),
                         url(r'add/$', 'addfile', name = 'file_manage.addfile'),
                         url(r'del/(?P<file_id>\d+)/$', 'delfile', name = 'file_manage.delfile'),
                         #url(r'^(?P<nomber>\d+)/+task/',include('task.urls')),
