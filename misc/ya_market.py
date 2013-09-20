577a22a3943ca7b2cc6ce8fbac3ca250a11beda7
Daiver <ra22341@ya.ru>
2013-09-20 18:05:14 +0400

    extract modelid

--- misc/ya_market.py
+++ misc/ya_market.py
@@ -1,15 +1,19 @@
 import sys
 from grab import Grab
 
-def ym_search(q):
+def ym_search(q): #fix it!
     #pageaddr ='http://google.com/search?hl=en&as_q=%s&num=%s&as_qdr=%s' % ('+'.join(q.split()), str(num),'')
     pageaddr ='http://market.yandex.ru/search.xml?text=%s' % ('+'.join(q.split()))
     #print pageaddr
     g = Grab()
     g.go(pageaddr)
-    title = g.doc.select('//div[@class="b-offers b-offers_type_guru b-offers_type_guru_mix"]/div/h3/a').text()
-    href = g.doc.select('//div[@class="b-offers b-offers_type_guru b-offers_type_guru_mix"]/div/h3/a/@href').text()
-    return (title, href)
+    try:
+        title = g.doc.select('//div[@class="b-offers b-offers_type_guru b-offers_type_guru_mix"]/div/h3/a').text()
+        href = g.doc.select('//div[@class="b-offers b-offers_type_guru b-offers_type_guru_mix"]/div/h3/a/@href').text()
+        modelid = href[href.find('modelid=') + len("modelid="):href.find('&hid')]
+        return (title, modelid, 'http://market.yandex.ru' + href)
+    except:
+        return ()
     #print title
     #print href
 
@@ -21,4 +25,5 @@ if __name__ == '__main__':
         print ym_search(sys.argv[1])
     else:
         print ym_search("4710937382310")
+        #print ym_search("47109370")
         #print ym_search("4605922006695")
