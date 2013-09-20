26afbac49d348d2d38e52bcd3bb08ebd14cb259e
Daiver <ra22341@ya.ru>
2013-09-20 18:38:03 +0400

    add comments

--- misc/ya_market.py
+++ misc/ya_market.py
@@ -12,7 +12,7 @@ def ym_search(q): #fix it!
         href = g.doc.select('//div[@class="b-offers b-offers_type_guru b-offers_type_guru_mix"]/div/h3/a/@href').text()
         modelid = href[href.find('modelid=') + len("modelid="):href.find('&hid')]
         return (title, modelid, 'http://market.yandex.ru' + href)
-    except:
+    except grab.error.DataNotFound as e:
         return ()
     #print title
     #print href
@@ -20,10 +20,36 @@ def ym_search(q): #fix it!
     #hrefs = g.doc.select('//div/h3/a/@href').text_list()
     #return zip(titles, hrefs)
 
+def ym_review(modelid):
+    pageaddr = "http://market.yandex.ru/product/%s/reviews" % modelid
+    g = Grab()
+    g.go(pageaddr)
+    try:
+        res = g.doc.select('//div[@class="b-aura-review__verdict"]/div').text_list()
+        def make_groups(lst):
+            res = []
+            i = 2
+            while i < len(lst):
+                res.append([lst[i - 2], lst[i - 1], lst[i]])
+                i += 3
+            return res
+
+        return make_groups(res)
+        
+    except:
+        return []
+
 if __name__ == '__main__':
     if len(sys.argv) > 1:
         print ym_search(sys.argv[1])
     else:
-        print ym_search("4710937382310")
+        ans = ym_search("4710937382310")
+        ans2 = ym_review(ans[1])
+        print ans
+        for x in ans2: 
+            print '>>>>'
+            for y in x:
+                print y
+        
         #print ym_search("47109370")
         #print ym_search("4605922006695")
